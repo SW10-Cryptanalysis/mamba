6631fa79d03bd8c9decec07bc5d3690ef9d1f3f4
Jacob Terpe Woods <jacobterpewoods@gmail.com>
2026-04-29 14:01:40 +0200

    hotfix: run_training update

--- run_training.sh
+++ run_training.sh
@@ -10,9 +10,11 @@
 # Navigate to project root
 cd /ceph/project/SW10-CausalLM/mamba
 mkdir -p logs
+mkdir -p .triton_cache
+mkdir -p .tmp
 # Define paths for clarity
 CONTAINER="./pytorch_25.03-py3.sif"
-VENV_DIR="./mamba-2-pt2503"
+VENV_DIR="$HOME/.venvs/mamba"
 MASTER_PORT=$((10000 + $RANDOM % 20000))
 echo "Starting job ${SLURM_JOB_ID} on $(hostname) at $(date)"
 # --- RUN TRAINING INSIDE SINGULARITY ---
@@ -21,6 +23,8 @@ echo "Starting job ${SLURM_JOB_ID} on $(hostname) at $(date)"
 # Inside your run_training.sh, replace the singularity block with this:
 # Define the host library path
 HOST_LIB_PATH="/usr/lib/x86_64-linux-gnu"
+
+TRAIN_ARGS="$@"
 
 singularity exec --nv \
     --pwd /ceph/project/SW10-CausalLM/mamba \
@@ -33,9 +37,12 @@ singularity exec --nv \
         # This helps the compiler find necessary headers
         export CPATH=\"/usr/local/cuda/include:\$CPATH\"
 
+        export TRITON_CACHE_DIR=\"/ceph/project/SW10-CausalLM/mamba/.triton_cache\"
+        export TMPDIR=\"/ceph/project/SW10-CausalLM/mamba/.tmp\"
+
         # This tells the system where to find the library at runtime
         export LD_LIBRARY_PATH=\"/.singularity.d/libs:\$LD_LIBRARY_PATH\"
         export OMP_NUM_THREADS=8
         torchrun --nproc_per_node=4 --master_port=$MASTER_PORT \
-            -m uv run --active -m src.train "$@" 2>&1
+            -m src.train $TRAIN_ARGS 2>&1
 " | tee -a logs/train_live_${SLURM_JOB_ID}.log
